7033842b93c00821463d67c90fc33651de0d45af
Cheolhoon Choi <choi4879@hawaii.edu>
2023-01-06 03:42:52 +0900

    게시판 페이징 기능 구현

--- study/src/main/java/kr/co/controller/BoardController.java
+++ study/src/main/java/kr/co/controller/BoardController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import kr.co.service.BoardService;
 import kr.co.vo.BoardVO;
+import kr.co.vo.Criteria;
+import kr.co.vo.PageMaker;
 
 @Controller
 @RequestMapping("/board/*")
@@ -35,18 +37,23 @@ public class BoardController {
 		
 		service.write(boardVO);
 		
-		return "redirect:/board/list" ;
+		return "redirect:/board/list";
 	}
 	
 	// 게시판 목록 조회
 	@RequestMapping(value = "/list", method = RequestMethod.GET)
-	public String list(Model model) throws Exception{
+	public String list(Model model, Criteria cri) throws Exception{
 		logger.info("list");
 		
-		model.addAttribute("list",service.list());
+		model.addAttribute("list", service.list(cri));
 		
-		return "board/list";
+		PageMaker pageMaker = new PageMaker();
+		pageMaker.setCri(cri);
+		pageMaker.setTotalCount(service.listCount());
 		
+		model.addAttribute("pageMaker", pageMaker);
+		
+		return "board/list";		
 	}
 	
 	// 게시판 조회
